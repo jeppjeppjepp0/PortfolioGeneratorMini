2e4eaf0fcd9edb3e6e58ba420ce8488b26b75114
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-19 13:33:54 -0600

    finished append

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
+
 inquirer
 .prompt([
     {
@@ -31,33 +33,53 @@ inquirer
 ])
 .then((response) => {
     // clear file
-    fs.writeFile('index.html', `
+    fs.writeFileSync('index.html', `
+<!DOCTYPE html>
+<html lang="en">
     <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Portfolio of ${response.username}</title>
     <link rel="stylesheet" href="./assets/css/style.css"/>
-    link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     `, (err) =>
     err ? console.error(err) : console.log('Head logged!'));
 
     // name
-    fs.appendFile('index.html', `
-    <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">This is the Portfolio of ${response.username}</a>
-    </div>
-    </nav>
-    ` , (err) =>
-    err ? console.error(err) : console.log('Body logged!'));
     // location
     // bio
     // linkedin
     // github
-
     fs.appendFile('index.html', `
-    </body>
+    <main>
+    <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand">This is the Portfolio of ${response.username}</a>
+      <p class="navbar-brand">From: ${response.location}</p>
+    </div>
+    </nav>
+
+    <ul class="list-group">
+        <li class="list-group-item">A fun fact about ${response.username} is: ${response.bio}</li>
+        <li class="list-group-item">Visit at:</li>
+        <li class="list-group-item">
+        <a href="https://github.com/${response.github}/">${response.github}</a>
+        </li>
+        <li class="list-group-item">
+        ${response.linkedin}
+        </li>
+        <li class="list-group-item">And a fifth one</li>
+    </ul>
+
+
+    </main>
+    ` , (err) =>
+    err ? console.error(err) : console.log('Body logged!'));
+    
+    fs.appendFile('index.html', `
+</body>
+</html>
     ` , (err) =>
     err ? console.error(err) : console.log('End File logged!'));
     
